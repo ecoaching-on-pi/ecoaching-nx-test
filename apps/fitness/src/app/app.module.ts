6f548cfcd9507da4d2f6754f234b11b98a970839
judith <ceogames2014@gmail.com>
2023-09-09 11:04:53 +0200

    add @angular/fire

--- apps/fitness/src/app/app.module.ts
+++ apps/fitness/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { FitnessComponentsModule } from '@ecoaching-on-pi/fitness/components';
 import { MaterialModule } from './material.module';
 import { SharedUiModule } from '@ecoaching-on-pi/shared/ui';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -18,6 +23,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FitnessComponentsModule,
     SharedUiModule,
     MaterialModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
   ],
 
   bootstrap: [AppComponent],
